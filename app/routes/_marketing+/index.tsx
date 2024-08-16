a4aee6a0cf2849a37de74a0a7f2bb4d3219ad22b
Devon Neill <35456245+devneill@users.noreply.github.com>
2024-08-16 16:27:27 +0200

    feat: add logo and replace epic notes with getfranki

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -1,4 +1,5 @@
 import { type MetaFunction } from '@remix-run/node'
+import { Icon } from '#app/components/ui/icon.js'
 import {
 	Tooltip,
 	TooltipContent,
@@ -8,7 +9,7 @@ import {
 import { cn } from '#app/utils/misc.tsx'
 import { logos } from './logos/logos.ts'
 
-export const meta: MetaFunction = () => [{ title: 'Epic Notes' }]
+export const meta: MetaFunction = () => [{ title: 'GetFranki' }]
 
 // Tailwind Grid cell classes lookup
 const columnClasses: Record<(typeof logos)[number]['column'], string> = {
@@ -36,36 +37,25 @@ export default function Index() {
 						href="https://www.epicweb.dev/stack"
 						className="animate-slide-top [animation-fill-mode:backwards] xl:animate-slide-left xl:[animation-delay:0.5s] xl:[animation-fill-mode:backwards]"
 					>
-						<svg
-							className="size-20 text-foreground xl:-mt-4"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 65 65"
-						>
-							<path
-								fill="currentColor"
-								d="M39.445 25.555 37 17.163 65 0 47.821 28l-8.376-2.445Zm-13.89 0L28 17.163 0 0l17.179 28 8.376-2.445Zm13.89 13.89L37 47.837 65 65 47.821 37l-8.376 2.445Zm-13.89 0L28 47.837 0 65l17.179-28 8.376 2.445Z"
-							></path>
-						</svg>
+						<Icon name="logo" title="logo" className="size-36" />
 					</a>
-					<h1
+					<div
 						data-heading
-						className="mt-8 animate-slide-top text-4xl font-medium text-foreground [animation-delay:0.3s] [animation-fill-mode:backwards] md:text-5xl xl:mt-4 xl:animate-slide-left xl:text-6xl xl:[animation-delay:0.8s] xl:[animation-fill-mode:backwards]"
+						className="animate-slide-top text-4xl font-medium text-foreground [animation-delay:0.3s] [animation-fill-mode:backwards] md:text-5xl xl:mt-4 xl:animate-slide-left xl:text-6xl xl:[animation-delay:0.8s] xl:[animation-fill-mode:backwards]"
 					>
-						<a href="https://www.epicweb.dev/stack">The Epic Stack</a>
-					</h1>
+						<Icon name="logo-text" title="logo text" className="h-40 w-72" />
+					</div>
 					<p
 						data-paragraph
 						className="mt-6 animate-slide-top text-xl/7 text-muted-foreground [animation-delay:0.8s] [animation-fill-mode:backwards] xl:mt-8 xl:animate-slide-left xl:text-xl/6 xl:leading-10 xl:[animation-delay:1s] xl:[animation-fill-mode:backwards]"
 					>
-						Check the{' '}
+						Run your events,{' '}
 						<a
 							className="underline hover:no-underline"
 							href="https://github.com/epicweb-dev/epic-stack/blob/main/docs/getting-started.md"
 						>
-							Getting Started guide
-						</a>{' '}
-						file for how to get your project off the ground!
+							in one place.
+						</a>
 					</p>
 				</div>
 				<ul className="mt-16 flex max-w-3xl flex-wrap justify-center gap-2 sm:gap-4 xl:mt-0 xl:grid xl:grid-flow-col xl:grid-cols-5 xl:grid-rows-6">
