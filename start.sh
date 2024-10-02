f2f2e355156e571ea44ff4c7ce7c5120eaf71d01
Devon Neill <35456245+devneill@users.noreply.github.com>
2024-10-02 14:26:19 +0200

    move start script to root

---
+++ start.sh
@@ -0,0 +1,7 @@
+# Configure AWS CLI
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws configure set endpoint_url $S3_ENDPOINT_URL
+
+# Start the application
+exec litefs mount
