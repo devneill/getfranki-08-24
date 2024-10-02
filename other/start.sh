4e3febf575ec1c8c3062e352b44f618faf7ba7fb
Devon Neill <35456245+devneill@users.noreply.github.com>
2024-10-02 14:20:47 +0200

    remove region

--- other/start.sh
+++ other/start.sh
@@ -1,7 +1,6 @@
 # Configure AWS CLI
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-aws configure set default.region $AWS_DEFAULT_REGION
 aws configure set endpoint_url $S3_ENDPOINT_URL
 
 # Start the application
