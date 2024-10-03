76d7ef4c9905be925f76f9783dc49481a619a0e6
Devon Neill <35456245+devneill@users.noreply.github.com>
2024-10-03 11:21:51 +0200

    add explicit endpoint url

--- other/backup_db.sh
+++ other/backup_db.sh
@@ -5,4 +5,4 @@ litefs export -name sqlite.db ./backups/backup-`date +%M`.db
 gzip ./backups/backup-`date +%M`.db
 
 # Daily backup
-aws s3 cp ./backups/backup-`date +%M`.db.gz s3://backups-sinp/backup-`date +%M`.db.gz
+aws s3 cp ./backups/backup-`date +%M`.db.gz s3://backups-sinp/backup-`date +%M`.db.gz --endpoint-url=$AWS_ENDPOINT_URL_S3
