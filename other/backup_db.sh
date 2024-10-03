71da477efc4da9982f5f76306eba17639d9b4e70
Devon Neill <35456245+devneill@users.noreply.github.com>
2024-10-03 10:00:16 +0200

    use minutes temp

--- other/backup_db.sh
+++ other/backup_db.sh
@@ -1,8 +1,8 @@
 # Make a snapshot of the current db
-litefs export -name sqlite.db ./backups/backup-`date +%d`.db
+litefs export -name sqlite.db ./backups/backup-`date +%M`.db
 
 # Compress the file.
-gzip ./backups/backup-`date +%d`.db
+gzip ./backups/backup-`date +%M`.db
 
 # Daily backup
-aws s3 cp ./backups/backup-`date +%d`.db.gz s3://backups-sinp/backup-`date +%d`.db.gz
+aws s3 cp ./backups/backup-`date +%M`.db.gz s3://backups-sinp/backup-`date +%M`.db.gz
