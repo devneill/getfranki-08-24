93dc9931b0900442551e36f2da0f0b2d5efbdfef
Devon Neill <35456245+devneill@users.noreply.github.com>
2024-10-04 08:55:03 +0200

    make db restore source named by minute

--- other/restore_db.sh
+++ other/restore_db.sh
@@ -1,8 +1,8 @@
 # Fetch db backup
-aws s3 cp s3://backups-sinp/backup-`date +%d`.db.gz ./backups/source-`date +%d`.db.gz  --endpoint-url=$AWS_ENDPOINT_URL_S3
+aws s3 cp s3://backups-sinp/backup-`date +%d`.db.gz ./backups/source-`date +%M`.db.gz  --endpoint-url=$AWS_ENDPOINT_URL_S3
 
 # Decompress the file.
-gunzip ./backups/source-`date +%d`.db.gz
+gunzip ./backups/source-`date +%M`.db.gz
 
 # Make a snapshot of the current db
-litefs import -name sqlite.db ./backups/source-`date +%d`.db
+litefs import -name sqlite.db ./backups/source-`date +%M`.db
