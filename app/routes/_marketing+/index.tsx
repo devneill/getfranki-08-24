c8229d21cd07476c50a4d11817065eb2cbc657c8
Devon Neill <35456245+devneill@users.noreply.github.com>
2024-10-07 14:27:23 +0200

    polishg

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -33,33 +33,34 @@ export async function loader({ request }: LoaderFunctionArgs) {
 	const like = `%${searchTerm ?? ''}%`
 	const categoryFilter = category ? `%${category}%` : '%'
 
-	const rawUsers = await prisma.$queryRaw`SELECT
-   User.id,
-   User.username,
-   User.name,
-   User.about,
-   UserImage.id AS imageId,
-   GROUP_CONCAT(DISTINCT C.name) AS categories,
-   r.name as role
-FROM User
-LEFT JOIN UserImage ON User.id = UserImage.userId
-JOIN _RoleToUser ru ON User.id = ru.b
-JOIN Role r ON ru.a = r.id
-JOIN _CategoryToUser cu ON User.id = cu.b
-JOIN Category c ON cu.a = c.id
-WHERE role LIKE "supplier"
-AND (User.username LIKE ${like} OR User.name LIKE ${like})
-AND C.name LIKE ${categoryFilter}
-GROUP BY User.id
-ORDER BY (
-    SELECT Event.updatedAt
-		FROM Event
-		WHERE Event.ownerId = User.id
-		ORDER BY Event.updatedAt DESC
-		LIMIT 1
-) DESC
-LIMIT 50
-`
+	const rawUsers = await prisma.$queryRaw`
+	 SELECT
+     User.id,
+     User.username,
+     User.name,
+     User.about,
+     UserImage.id AS imageId,
+     GROUP_CONCAT(DISTINCT C.name) AS categories,
+     r.name as role
+     FROM User
+     LEFT JOIN UserImage ON User.id = UserImage.userId
+     JOIN _RoleToUser ru ON User.id = ru.b
+     JOIN Role r ON ru.a = r.id
+     JOIN _CategoryToUser cu ON User.id = cu.b
+     JOIN Category c ON cu.a = c.id
+     WHERE role LIKE "supplier"
+     AND (User.username LIKE ${like} OR User.name LIKE ${like})
+     AND C.name LIKE ${categoryFilter}
+     GROUP BY User.id
+     ORDER BY (
+     SELECT Event.updatedAt
+    	FROM Event
+    	WHERE Event.ownerId = User.id
+    	ORDER BY Event.updatedAt DESC
+    	LIMIT 1
+     ) DESC
+     LIMIT 50
+    `
 
 	const result = UserSearchResultsSchema.safeParse(rawUsers)
 
@@ -83,6 +84,10 @@ export default function Index() {
 		formMethod: 'GET',
 		formAction: '/',
 	})
+
+	if (data.status === 'error') {
+		console.error(data.error)
+	}
 
 	return (
 		<div className="container mb-48 mt-14 flex flex-col items-center justify-center gap-6">
