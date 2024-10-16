8010dfabdbb636707c58100816d041cdb853b95b
Devon Neill <35456245+devneill@users.noreply.github.com>
2024-10-16 19:16:56 +0200

    add plausible to CSP

--- server/index.ts
+++ server/index.ts
@@ -115,6 +115,7 @@ app.use(
 				'connect-src': [
 					MODE === 'development' ? 'ws:' : null,
 					process.env.SENTRY_DSN ? '*.sentry.io' : null,
+					'*.plausible.io',
 					"'self'",
 				].filter(Boolean),
 				'font-src': ["'self'"],
